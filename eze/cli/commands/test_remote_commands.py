1b8ff69287fe2e5a9488a9e04b7f3e608991916f
mgonzales-rs <melissa.gonzales@riversafe.co.uk>
2021-08-05 15:44:01 +0100

    ab-604: updates fixes

--- eze/cli/commands/test_remote_commands.py
+++ eze/cli/commands/test_remote_commands.py
@@ -28,22 +28,24 @@ from eze.core.engine import EzeCore
 @click.option(
     "--url",
     "-u",
-    help="Especify the url of the remote repository to run scan. ex https://user:pass@github.com/repo-url",
+    help="Specify the url of the remote repository to run scan. ex https://user:pass@github.com/repo-url",
     required=True,
 )
 @click.option(
     "--online",
     is_flag=True,
-    help="Especify if you want to run the test online instead of locally",
+    help="Specify if you want to run the test online instead of locally",
 )
 def test_remote_commands(state, config_file: str, scan_type, url: str, online: bool) -> None:
     """Eze run scan in a remote repository"""
     if online:
-        api_url = "https://lff3adj48f.execute-api.eu-west-2.amazonaws.com/staging/v1/api/remote-scan"
+        api_url = os.environ.get("EZE_REMOTE_SCAN_ENDPOINT", "")
         data = {"remote-url": url}
         try:
             req = urllib.request.Request(
-                api_url, data=pretty_print_json(data).encode("utf-8"), headers={"Authorization": "---"}
+                api_url,
+                data=pretty_print_json(data).encode("utf-8"),
+                headers={"Authorization": os.environ.get("EZE_APIKEY", "")},
             )
             with urllib.request.urlopen(req) as response:
                 url_response = response.read()
