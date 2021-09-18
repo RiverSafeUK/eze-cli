9c1ae077fe3d9d2dee78228fac4caf6e4b81729a
Caleb Eghan <caleb.eghan@riversafe.co.uk>
2021-09-18 02:45:17 +0100

    ab-778-add-test-for-json-return-condition-io-py

--- tests/utils/test_io.py
+++ tests/utils/test_io.py
@@ -253,3 +253,17 @@ def test_delete_file(tmp_path):
 
     delete_file(file_name)
     assert os.path.exists(file_name) == 0
+
+
+'''Test if json return condition works'''
+
+
+def test_json_return(tmp_path):
+    json_file = tmp_path / "path/test_file.json"
+    json_file.parent.mkdir()
+    json_file.touch()
+
+    expected = []
+    sample = load_json(json_file)
+    assert expected == sample
+    os.remove(json_file)
