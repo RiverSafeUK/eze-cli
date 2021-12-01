8113f804eff1a263fe711d31524c9a9d7ddfb77c
Michael <Michael.obineche@riversafe.co.uk>
2021-12-01 09:21:58 +0000

    ab-912: unit tests added when semgrep ran without it locally installed

--- tests/plugins/tools/test_semgrep.py
+++ tests/plugins/tools/test_semgrep.py
@@ -4,8 +4,10 @@ from unittest import mock
 import pytest
 
 from eze.plugins.tools.semgrep import SemGrepTool
+from eze.utils.error import EzeError
 from eze.utils.io import create_tempfile_path
 from tests.plugins.tools.tool_helper import ToolMetaTestBase
+from tests.__test_helpers__.mock_helper import mock_run_cmd
 
 
 class TestSemGrepTool(ToolMetaTestBase):
@@ -90,3 +92,35 @@ class TestSemGrepTool(ToolMetaTestBase):
 
         # Test run calls correct program
         await self.assert_run_scan_command(input_config, expected_cmd, mock_subprocess_run)
+
+    @mock.patch(
+        "eze.plugins.tools.semgrep.extract_cmd_version",
+        mock.MagicMock(return_value="ModuleNotFoundError: No module named 'resource'"),
+    )
+    def test_check_semgrep_not_installed(self):
+        # Given
+        expected_output = "ModuleNotFoundError: No module named 'resource'"
+        # When
+        output = SemGrepTool.check_installed()
+        # Then
+        assert expected_output in output
+
+    @mock.patch("eze.utils.cli.run_cmd")
+    @mock.patch("eze.utils.cli.is_windows_os", mock.MagicMock(return_value=True))
+    @pytest.mark.asyncio
+    async def test_run_scan_without_semgrep_locally_installed_raise_eze_error(self, mocked_run_cmd):
+        # Given
+        input_config = {}
+        input_stdout = "Semgrep ran into an issue"
+        # When
+        mock_run_cmd(mocked_run_cmd, input_stdout, "ModuleNotFoundError: No module named 'resource'")
+        expected_error_message = """[semgrep] semgrep crashed while running, this is likely because semgrep doesn't support native windows yet
+
+As of 2021 semgrep support for windows is limited, until support added you can use eze inside wsl2 to run semgrep on windows
+https://github.com/returntocorp/semgrep/issues/1330"""
+        # Test run calls correct program
+        testee = self.ToolMetaClass(input_config)
+        # Then
+        with pytest.raises(EzeError) as thrown_exception:
+            await testee.run_scan()
+        assert expected_error_message == thrown_exception.value.message
