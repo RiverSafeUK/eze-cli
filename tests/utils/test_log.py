ad778a00dabf61cd9869ece88c3228fabc0e64e1
Michael <Michael.obineche@riversafe.co.uk>
2021-12-08 12:04:16 +0000

    ab-897: added unit tests for tests/utils/test_log.py

---
+++ tests/utils/test_log.py
@@ -0,0 +1,119 @@
+"""Unit test for tests/utils/test_log.py"""
+from eze.utils.log import log, log_debug, log_error
+from tests.__test_helpers__.mock_helper import mock_print, unmock_print, mock_print_stderr, unmock_print_stderr
+
+
+def test_log():
+    """Test log output to stdout"""
+
+    # Setup
+    mocked_print_output = mock_print()
+
+    # Input
+    test_input = "Echo 123"
+    log(test_input)
+
+    # Teardown
+    unmock_print()
+
+    # Test
+    output = mocked_print_output.getvalue()
+    expected_output = "Echo 123\n"
+    assert output == expected_output
+
+
+def test_log_does_print_to_stderr():
+    """Test log output to stderr"""
+
+    # Setup
+    mocked_print_output_stderr = mock_print_stderr()
+
+    # Input
+    test_input = "Echo 123"
+    log(test_input)
+
+    # Teardown
+    unmock_print_stderr()
+
+    # Test
+    output_stderr = mocked_print_output_stderr.getvalue()
+    expected_output_stderr = ""
+    assert output_stderr == expected_output_stderr
+
+
+# Not sure how to run test in --debug mode to get it to print to stdout
+# @debug_option
+# def test_log_debug_in_debug():
+#     """Test log_debug output to stdout in debug mode"""
+
+#     # Setup
+#     mocked_print_output = mock_print()
+
+#     # Input
+#     test_input = "Echo 123"
+#     log_debug(test_input)
+
+#     # Teardown
+#     unmock_print()
+
+#     # Test
+#     output = mocked_print_output.getvalue()
+#     expected_output = "Echo 123\n"
+#     assert output == expected_output
+
+
+def test_log_debug_not_in_debug_mode():
+    """Test log_debug output to stdout when its not in debug mode"""
+
+    # Setup
+    mocked_print_output = mock_print()
+
+    # Input
+    test_input = "Echo 123"
+    log_debug(test_input)
+
+    # Teardown
+    unmock_print()
+
+    # Test
+    output = mocked_print_output.getvalue()
+    expected_output = ""
+    assert output == expected_output
+
+
+def test_log_error():
+    """Test log_error output to stderr"""
+
+    # Setup
+    mocked_print_output_stderr = mock_print_stderr()
+
+    # Input
+    test_input = "Echo 123"
+    log_error(test_input)
+
+    # Teardown
+    unmock_print_stderr()
+
+    # Test
+    output_stderr = mocked_print_output_stderr.getvalue()
+    expected_output_stderr = "Echo 123\n"
+    assert output_stderr == expected_output_stderr
+
+
+def test_log_error_does_not_print_to_stdout():
+    """Test log_error output to stdout"""
+
+    # Setup
+    mocked_print_output_stdout = mock_print()
+
+    # Input
+    test_input = "Echo 123"
+    log_error(test_input)
+
+    # Teardown
+    unmock_print()
+
+    # Test
+    output_stdout = mocked_print_output_stdout.getvalue()
+    expected_output_stdout = ""
+    assert output_stdout == expected_output_stdout
