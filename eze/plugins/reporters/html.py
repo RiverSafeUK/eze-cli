5349ec5d960bc4d74f47aee58ac52ef3f8a9d965
mgonzales-rs <melissa.gonzales@riversafe.co.uk>
2022-03-01 09:48:17 +0000

    ab#1043: remove stylesheet

--- eze/plugins/reporters/html.py
+++ eze/plugins/reporters/html.py
@@ -41,11 +41,6 @@ By default set to eze_report.html""",
             <html lang="en">
             <head>
                 <title>Eze Scanning report</title>
-                <link
-                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-                    rel="stylesheet"
-                    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-                    crossorigin="anonymous">
             </head>
 
             <body>
